37684484fd96f70696d884c934d70ee68ac0802e
Aleh Komar <komar.aleh@gmail.com>
2023-11-20 11:08:30 +0100

    198

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import { useState } from 'react'
+import menu from './data'
 import Title from './Title'
+import Menu from './Menu'
+
+const allCategories = ['all', ...new Set(menu.map((item) => item.category))]
 
 const App = () => {
+  const [menuItems, SetMenuItems] = useState(menu)
+  const [categories, setCategories] = useState(allCategories)
   return (
     <main>
       <section className='menu'>
         <Title text='our menu' />
+        <Menu items={menuItems} />
       </section>
     </main>
   )
